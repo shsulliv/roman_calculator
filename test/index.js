07bff08cd9185fcd461eaeefef26cdf5308ab8b8
shsulliv <shsulliv@gmail.com>
2016-03-26 13:12:46 +0100

    Improved wording on tests

--- test/index.js
+++ test/index.js
@@ -9,28 +9,28 @@ test('The calculator adds two Roman numerals together', t => {
   t.is(calculator('I', 'I'), 'II');
 });
 
-test('The calculator represents the numeral "four" correctly', t => {
+test('The calculator returns the numeral "four" correctly', t => {
   t.is(calculator('III', 'I'), 'IV');
 });
 
-test('The calculator represents the numeral "five" correctly', t => {
+test('The calculator returns the numeral "five" correctly', t => {
   t.is(calculator('III', 'II'), 'V');
   t.is(calculator('IV', 'I'), 'V');
   t.is(calculator('I', 'IV'), 'V');
 });
 
-test('The calculator represents the numeral "six" correctly', t => {
+test('The calculator returns the numeral "six" correctly', t => {
   t.is(calculator('V', 'I'), 'VI');
   t.is(calculator('III', 'III'), 'VI');
   t.is(calculator('IV', 'II'), 'VI');
   t.is(calculator('II', 'IV'), 'VI');
 });
 
-test('The calculator represents the numeral "fifty" correctly', t => {
+test('The calculator returns the numeral "fifty" correctly', t => {
   t.is(calculator('XX', 'XXX'), 'L');
 });
 
-test('The calculator represents numerals over "fifty" correctly', t => {
+test('The calculator returns numerals over "fifty" correctly', t => {
   t.is(calculator('CCCLXIX', 'DCCCXLV'), 'MCCXIV');
   t.is(calculator('CXLVII', 'MCV'), 'MCCLII');
 });
